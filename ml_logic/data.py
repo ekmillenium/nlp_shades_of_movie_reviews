eea16931a3b0510b12035ae9890500440d92d945
Eric Kang <eric.kang@hotmail.fr>
2023-03-08 10:26:06 +0100

    amended basic preproc function

--- ml_logic/data.py
+++ ml_logic/data.py
@@ -16,27 +16,30 @@ def basic_preprocessing(sentence):
     '''
 
     ################## Basic preprocessing #######################################################################
-    
-    # remove the line about "how many people found this useful" at the end of some reviews
+
+    # remove the lines about "how many people found this useful" at the end of some reviews
+    sentence = re.sub("Permalink", "", sentence)
+    sentence = re.sub("Was this review helpful?", "", sentence)
+    sentence = re.sub("Sign in to vote.", "", sentence)
     sentence = re.sub("\d+ out of \d+ found this helpful", "", sentence)
-    
+
     # Remove urls from text
     sentence = re.sub(r"http\S+", "", sentence)
-    
+
     #remove html tags if there are any
     sentence = re.sub(re.compile('<.*?>'), '', sentence)
-    
+
     # lowercase characters
     sentence = sentence.lower()
-    
+
     # remove numbers
     sentence = ''.join(char for char in sentence if not char.isdigit())
-    
+
     # remove punctuation
     for punctuation in string.punctuation:
         sentence = sentence.replace(punctuation, ' ')
-    
+
     #remove extra space
     sentence = " ".join(sentence.split())
-    
+
     return sentence
