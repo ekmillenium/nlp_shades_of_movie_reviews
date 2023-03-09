3dd77ea64b31f594f610b7998962164c33b12e57
Arthur Dhuy <a.dhuy@orange.fr>
2023-03-09 16:59:14 +0100

    Fix preprocessing error

--- ml_logic/data.py
+++ ml_logic/data.py
@@ -35,22 +35,20 @@ class Preprocessing():
         - remove extra space
         '''
         # remove the lines about "how many people found this useful" at the end of some reviews
-        clean_review = re.sub("Permalink", "", self.review)
-        clean_review = re.sub("Was this review helpful?", "", self.review)
-        clean_review = re.sub("Sign in to vote.", "", self.review)
-        clean_review = re.sub("\d+ out of \d+ found this helpful", "", self.review)
+        self.review = re.sub("Permalink", "", self.review)
+        self.review = re.sub("Was this review helpful?", "", self.review)
+        self.review = re.sub("Sign in to vote.", "", self.review)
+        self.review = re.sub("\d+ out of \d+ found this helpful", "", self.review)
 
         # Remove urls from text
-        clean_review = re.sub(r"http\S+", "", self.review)
+        self.review = re.sub(r"http\S+", "", self.review)
 
         #remove html tags if there are any
-        clean_review = re.sub(re.compile('<.*?>'), '', self.review)
+        self.review = re.sub(re.compile('<.*?>'), '', self.review)
 
         #remove extra space
-        clean_review = " ".join(self.review.split())
-
-        return clean_review
-
+        self.review = " ".join(self.review.split())
+        
 
     ### Preprocessing for BERT ###
 
@@ -66,31 +64,30 @@ class Preprocessing():
         - remove punctuation
         '''
         # remove the lines about "how many people found this useful" at the end of some reviews
-        clean_review = re.sub("Permalink", "", self.review)
-        clean_review = re.sub("Was this review helpful?", "", self.review)
-        clean_review = re.sub("Sign in to vote.", "", self.review)
-        clean_review = re.sub("\d+ out of \d+ found this helpful", "", self.review)
+        self.review = re.sub("Permalink", "", self.review)
+        self.review = re.sub("Was this review helpful?", "", self.review)
+        self.review = re.sub("Sign in to vote.", "", self.review)
+        self.review = re.sub("\d+ out of \d+ found this helpful", "", self.review)
 
         # Remove urls from text
-        clean_review = re.sub(r"http\S+", "", self.review)
+        self.review = re.sub(r"http\S+", "", self.review)
 
         #remove html tags if there are any
-        clean_review = re.sub(re.compile('<.*?>'), '', self.review)
+        self.review = re.sub(re.compile('<.*?>'), '', self.review)
 
         # lowercase characters
-        clean_review = self.review.lower()
+        self.review = self.review.lower()
 
         # remove numbers
-        clean_review = ''.join(char for char in self.review if not char.isdigit())
+        self.review = ''.join(char for char in self.review if not char.isdigit())
 
         # remove punctuation
         for punctuation in string.punctuation:
-            clean_review = self.review.replace(punctuation, ' ')
+            self.review = self.review.replace(punctuation, ' ')
 
         #remove extra space
-        clean_review = " ".join(self.review.split())
+        self.review = " ".join(self.review.split())
 
-        return clean_review
 
 
 class TargetBuilder():
